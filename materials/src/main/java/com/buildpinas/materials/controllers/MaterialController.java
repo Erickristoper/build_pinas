05f6d98ff359ca408047c33b87b5d9f81e840225
GreenPickles <a.n.royo@outlook.com>
2025-02-15 13:52:28 +0800

    change

--- materials/src/main/java/com/buildpinas/materials/controllers/MaterialController.java
+++ materials/src/main/java/com/buildpinas/materials/controllers/MaterialController.java
@@ -19,6 +19,8 @@ public class MaterialController {
 
     @GetMapping
     public List<Material> getMaterials(){
+
+        System.out.println("testing");
         return materialRepo.findAll();
     }
     @GetMapping("/{id}")
